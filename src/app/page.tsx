33de72c60f18e6da05075e8fe1afa17db991bff9
hyerijung <hyerijung0903@gmail.com>
2025-03-07 01:26:48 +0900

    seperate data file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,26 +5,6 @@ import {useState} from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-const recommendation = [
-    { id: 'intj', country: '마카오' },
-    { id: 'intp', country: '홍콩' },
-    { id: 'injf', country: '삿포로' },
-    { id: 'infp', country: '발리' },
-    { id: 'istj', country: '도쿄' },
-    { id: 'istp', country: '베트남' },
-    { id: 'isfj', country: '보홀' },
-    { id: 'isfp', country: '코타키나발루' },
-    { id: 'entj', country: '괌' },
-    { id: 'entp', country: '대만' },
-    { id: 'enfj', country: '푸켓' },
-    { id: 'enfp', country: '세부' },
-    { id: 'estj', country: '오사카' },
-    { id: 'estp', country: '파타야' },
-    { id: 'esfj', country: '보라카이' },
-    { id: 'esfp', country: '방콕' }
-];
-
-
 export default function Home() {
     const [text, setText] = useState("");
     const [isExist, setIsExist] = useState(true);
--- src/app/tests/[id]/page.tsx
+++ src/app/tests/[id]/page.tsx
@@ -1,51 +1,51 @@
-
-'use client';
-
-import React from "react";
-import { useRouter } from 'next/navigation';
-
-const questions = [
-  { id: 1, question: "당신은 새로운 사람들과 쉽게 어울리나요?", options: ["예", "아니요"] },
-  { id: 2, question: "계획적인 것을 좋아하시나요?", options: ["예", "아니요"] },
-  { id: 3, question: "감정보다 논리를 우선하나요?", options: ["예", "아니요"] },
-];
-
-export default function TestPage(props) {
-  const router = useRouter();
-  const questionIndex = parseInt(props.params.id)-1
-  const isLastQuestion = questionIndex === questions.length - 1;
-
-  // 배열 범위를 벗어나면 에러 방지
-  if (questionIndex < 0 || questionIndex >= questions.length) {
-    return <h1>존재하지 않는 질문입니다.</h1>;
-  }
-
-  function handleAnswer(answer: number) {
-    console.log('Answer: ' + answer);
-
-    if (isLastQuestion) {
-      router.push("/result"); // 마지막 질문이라면 결과 페이지로 이동
-    } else {
-      router.push(`/tests/${questionIndex + 2}`); // 다음 질문으로 이동
-    }
-  }
-
-  return (
-    <>
-      <h1>TEST PAGE</h1>
-      <div>ID: {props.params.id}</div>
-      <div>Question Index: {questionIndex}</div>
-
-      <div>{questions[questionIndex].question}</div>
-      <div>
-        {questions[questionIndex].options.map((option, index) => (
-          <div key={index}>
-            <button onClick={() => handleAnswer(index)}>
-              {option}
-            </button>
-          </div>
-        ))}
-      </div>
-    </>
-  );
-}
+//
+// 'use client';
+//
+// import React from "react";
+// import { useRouter } from 'next/navigation';
+//
+// const questions = [
+//   { id: 1, question: "당신은 새로운 사람들과 쉽게 어울리나요?", options: ["예", "아니요"] },
+//   { id: 2, question: "계획적인 것을 좋아하시나요?", options: ["예", "아니요"] },
+//   { id: 3, question: "감정보다 논리를 우선하나요?", options: ["예", "아니요"] },
+// ];
+//
+// export default function TestPage(props) {
+//   const router = useRouter();
+//   const questionIndex = parseInt(props.params.id)-1
+//   const isLastQuestion = questionIndex === questions.length - 1;
+//
+//   // 배열 범위를 벗어나면 에러 방지
+//   if (questionIndex < 0 || questionIndex >= questions.length) {
+//     return <h1>존재하지 않는 질문입니다.</h1>;
+//   }
+//
+//   function handleAnswer(answer: number) {
+//     console.log('Answer: ' + answer);
+//
+//     if (isLastQuestion) {
+//       router.push("/result"); // 마지막 질문이라면 결과 페이지로 이동
+//     } else {
+//       router.push(`/tests/${questionIndex + 2}`); // 다음 질문으로 이동
+//     }
+//   }
+//
+//   return (
+//     <>
+//       <h1>TEST PAGE</h1>
+//       <div>ID: {props.params.id}</div>
+//       <div>Question Index: {questionIndex}</div>
+//
+//       <div>{questions[questionIndex].question}</div>
+//       <div>
+//         {questions[questionIndex].options.map((option, index) => (
+//           <div key={index}>
+//             <button onClick={() => handleAnswer(index)}>
+//               {option}
+//             </button>
+//           </div>
+//         ))}
+//       </div>
+//     </>
+//   );
+// }
