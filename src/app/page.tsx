a76306c47c185a066eb3d82490810019e426ffb4
hyerijung <hyerijung0903@gmail.com>
2025-03-10 23:54:26 +0900

    remove unused module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import {useState} from "react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { recommendation } from '@/data/resultData';
 
