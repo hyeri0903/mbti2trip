2e7e0c6edaf0b8c8902e9792fc4651eab71ff035
hyerijung <hyerijung0903@gmail.com>
2025-04-30 22:54:09 +0900

    fix thumbnail to airplane

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
                 <meta name="description" content="MBTI에 맞는 해외 여행지를 추천합니다." />
                 <meta property="og:title" content="MBTI에 맞는 해외 여행지 추천"/>
                 <meta property="og:description" content="MBTI에 맞는 해외 여행지를 추천합니다."/>
-                <meta property="og:image" content="https://mbti-to-trip.vercel.app/thumbnail.jpg" />
+                <meta property="og:image" content="https://mbti-to-trip.vercel.app/airplane.png" />
                 <meta property="og:url" content="https://mbti-to-trip.vercel.app"/>
                 <meta property="og:type" content="website"/>
                 <meta property="og:site_name" content="MBTI to Trip"/>
@@ -96,7 +96,7 @@ export default function Home() {
                 <meta name="twitter:card" content="summary"/>
                 <meta name="twitter:title" content="MBTI에 맞는 해외 여행지 추천"/>
                 <meta name="twitter:description" content="MBTI에 맞는 해외 여행지를 추천합니다."/>
-                <meta name="twitter:image" content="https://mbti-to-trip.vercel.app/thumbnail.jpg"/>
+                <meta name="twitter:image" content="https://mbti-to-trip.vercel.app/airplane.png"/>
             </Head>
 
             <div className="min-h-screen flex flex-col items-center justify-center relative">
