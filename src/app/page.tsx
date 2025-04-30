7c3a5b19e1eaa94742d47ece3ba92d259266a50b
hyerijung <hyerijung0903@gmail.com>
2025-04-30 22:51:26 +0900

    썸네일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,17 +88,15 @@ export default function Home() {
                 <meta name="description" content="MBTI에 맞는 해외 여행지를 추천합니다." />
                 <meta property="og:title" content="MBTI에 맞는 해외 여행지 추천"/>
                 <meta property="og:description" content="MBTI에 맞는 해외 여행지를 추천합니다."/>
-                <meta property="og:image" content="/thumbnail.jpg" />
+                <meta property="og:image" content="https://mbti-to-trip.vercel.app/thumbnail.jpg" />
                 <meta property="og:url" content="https://mbti-to-trip.vercel.app"/>
                 <meta property="og:type" content="website"/>
                 <meta property="og:site_name" content="MBTI to Trip"/>
                 {/* twitter card */}
-        
+                <meta name="twitter:card" content="summary"/>
                 <meta name="twitter:title" content="MBTI에 맞는 해외 여행지 추천"/>
                 <meta name="twitter:description" content="MBTI에 맞는 해외 여행지를 추천합니다."/>
-                <meta name="twitter:image" content="thumbnail.jpg"/>
-                <meta property="twitter:url" content="https://mbti-to-trip.vercel.app"/>
-                <meta name="twitter:card" content="summary"/>
+                <meta name="twitter:image" content="https://mbti-to-trip.vercel.app/thumbnail.jpg"/>
             </Head>
 
             <div className="min-h-screen flex flex-col items-center justify-center relative">
