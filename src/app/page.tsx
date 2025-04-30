83e09d6596ee7e7c441ba28f6a4c7529a0b98904
hyerijung <hyerijung0903@gmail.com>
2025-04-30 22:36:35 +0900

    change thumbnail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,23 +84,21 @@ export default function Home() {
         <>
             <Head>
                 <title>MBTI에 맞는 해외 여행지 추천</title>
-                <link rel="icon" href="/api/favicon" type="image/png" />
                 <meta name="description" content="MBTI에 맞는 해외 여행지를 추천합니다." />
                 <meta property="og:title" content="MBTI에 맞는 해외 여행지 추천"/>
                 <meta property="og:description" content="MBTI에 맞는 해외 여행지를 추천합니다."/>
-                <meta property="og:image" content="https://mbti-to-trip.vercel.app/api/og" />
+                <meta property="og:image" content="https://mbti-to-trip.vercel.app/thumbnail.jpg" />
                 <meta property="og:image:width" content="1200" />
                 <meta property="og:image:height" content="630" />
-                <meta property="og:image:type" content="image/png" />
                 <meta property="og:url" content="https://mbti-to-trip.vercel.app"/>
                 <meta property="og:type" content="website"/>
                 <meta property="og:site_name" content="MBTI to Trip"/>
                 <meta name="twitter:card" content="summary_large_image"/>
                 <meta name="twitter:title" content="MBTI에 맞는 해외 여행지 추천"/>
                 <meta name="twitter:description" content="MBTI에 맞는 해외 여행지를 추천합니다."/>
-                <meta name="twitter:image" content="https://mbti-to-trip.vercel.app/api/og"/>
+                <meta name="twitter:image" content="https://mbti-to-trip.vercel.app/thumbnail.jpg"/>
                 <meta name="twitter:image:alt" content="MBTI 여행지 추천 썸네일" />
-                <meta name="twitter:site" content="@mbti2trip" />
+
             </Head>
 
             <div className="min-h-screen flex flex-col items-center justify-center relative">
